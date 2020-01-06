5f35d3af85a70952b464755b3dec55128691c6cf
rustam <rusyasoft@gmail.com>
2020-01-06 12:15:07 +0900

    - first trial with basic testings

--- src/gatling/scala/MyFirstTest.scala
+++ src/gatling/scala/MyFirstTest.scala
@@ -1,13 +1,40 @@
+import baseConfig.IdnMembershipApiSimulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class MyFirstTest extends Simulation {
+import scala.concurrent.duration.DurationInt
+
+class MyFirstTest extends IdnMembershipApiSimulation {
+//class MyFirstTest extends Simulation {
 
   // 1 Common HTTP Configuration
+//  val httpConf = http
+//    .baseURL("http://localhost:8080/app/")
+//    .header("Accept", "application/json")
+
+  def getMembershipBalance() = {
+//    repeat(3) {
+      exec(http("Get Balance")
+        .get("api/membership/v1/cash/balance")
+        .check(status.is(200)))
+//    }
+
+  }
 
   // 2 Scenario Definition
+  val scn = scenario("Membership Scenario")
+    .exec(getMembershipBalance())
 
   // 3 Load Scenario
+  setUp(
+//    scn.inject(atOnceUsers(11))
+    scn.inject(
+      nothingFor(5 seconds),
+      constantUsersPerSec(100) during (10 seconds)
+//      rampUsersPerSec(1) to (5) during (20 seconds)
+
+    )
+  ).protocols(httpConf)
 
 
 }
---
+++ src/gatling/scala/baseConfig/IdnMembershipApiSimulation.scala
@@ -0,0 +1,15 @@
+package baseConfig
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class IdnMembershipApiSimulation extends Simulation {
+  val LOGIN_STATUS = "ACTIVE"
+  val httpConf = http
+    .baseURL("http://idn-membership-dev.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
+    .header("Accept", "application/json")
+    .header("auth", "vYKif5ZR3PEGDsG_mC9ok7yjH6fWLlqnnQNfP0Zywqo=")
+    .header("user-id", "hihitest1")
+    .header("login-status", LOGIN_STATUS)
+
+}
