0e3b7fd1d734a4b1fc3e5ebadaff648a309be4e7
rustam <rusyasoft@gmail.com>
2020-01-06 14:59:49 +0900

    - 10 users request at once

--- src/gatling/scala/baseConfig/IdnMembershipApiSimulation.scala
+++ src/gatling/scala/baseConfig/IdnMembershipApiSimulation.scala
@@ -6,7 +6,8 @@ import io.gatling.http.Predef._
 class IdnMembershipApiSimulation extends Simulation {
   val LOGIN_STATUS = "ACTIVE"
   val httpConf = http
-    .baseURL("http://idn-membership-dev.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
+    //.baseURL("http://idn-membership-dev.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
+    .baseURL("http://IDN-Membership-Production.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
     .header("Accept", "application/json")
     .header("auth", "vYKif5ZR3PEGDsG_mC9ok7yjH6fWLlqnnQNfP0Zywqo=")
     .header("user-id", "hihitest1")
