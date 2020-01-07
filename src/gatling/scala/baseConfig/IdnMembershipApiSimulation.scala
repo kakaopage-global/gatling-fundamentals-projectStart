ca9122ce2a54d9a9ec4c318d631953e6efc3e77e
rustam <rusyasoft@gmail.com>
2020-01-07 16:32:42 +0900

    - divided to environments

--- src/gatling/scala/baseConfig/IdnMembershipApiSimulation.scala
+++ src/gatling/scala/baseConfig/IdnMembershipApiSimulation.scala
@@ -5,18 +5,50 @@ import io.gatling.core.body.CompositeByteArrayBody
 import io.gatling.http.Predef._
 
 class IdnMembershipApiSimulation extends Simulation {
+
+  def loadTestEnv: String = getProperty("LOAD_TEST_ENV", "dev")
+
+  private def getProperty(propertyName: String, defaultValue: String) = {
+    Option(System.getenv(propertyName))
+      .orElse(Option(System.getProperty(propertyName)))
+      .getOrElse(defaultValue)
+  }
+
   val LOGIN_STATUS = "ACTIVE"
-  val httpConf = http
-    .baseURL("http://idn-membership-dev.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
-//    .baseURL("http://IDN-Membership-Production.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
-    .header("Accept", "application/json")
-    .header("auth", "vYKif5ZR3PEGDsG_mC9ok7yjH6fWLlqnnQNfP0Zywqo=")
-//    .header("user-id", "hihitest1")
-    .header("login-status", LOGIN_STATUS)
+  val httpConf = if (loadTestEnv.equals("dev") ) {
+    println("----- dev ENVIRONMENT ---")
+    http
+      .baseURL("http://idn-membership-dev.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
+      .header("Accept", "application/json")
+      .header("auth", "vYKif5ZR3PEGDsG_mC9ok7yjH6fWLlqnnQNfP0Zywqo=")
+      .header("login-status", LOGIN_STATUS)
+  } else if (loadTestEnv.equals("prod") ) {
+    println("----- prod ENVIRONMENT ---")
+    http
+      .baseURL("http://IDN-Membership-Production.kgbh39purd.ap-southeast-1.elasticbeanstalk.com/")
+      .header("Accept", "application/json")
+      .header("auth", "vYKif5ZR3PEGDsG_mC9ok7yjH6fWLlqnnQNfP0Zywqo=")
+      .header("login-status", LOGIN_STATUS)
+  } else {
+    println("----- Unknown ENVIRONMENT --- : " + loadTestEnv)
+    http
+      .baseURL("http://localhost:8080/")
+      .header("Accept", "application/json")
+      .header("auth", "vYKif5ZR3PEGDsG_mC9ok7yjH6fWLlqnnQNfP0Zywqo=")
+      .header("login-status", LOGIN_STATUS)
+  }
 
-  val episodeId = 456
+  //  val episodeId = 7
 
-  val ticketPurchaseBody: CompositeByteArrayBody = StringBody(
+  val giftGiveEventCashBody = StringBody(
+    """{ "campaignId": 1,
+           "campaignName": "testCampaign",
+           "endDt": "2020-01-30T08:56:59Z",
+           "rewardAmount": 200,
+           "rewardId": 1,
+           "userId": "${id}" }""".stripMargin)
+
+  val ticketPurchaseBodyProd: CompositeByteArrayBody = StringBody(
     """{
           "contentId": 18,
           "ticketMap": {
@@ -25,10 +57,40 @@ class IdnMembershipApiSimulation extends Simulation {
           }
       }""".stripMargin)
 
-  val ticketUseQueryMap = Map(
+  val ticketPurchaseBodyDev: CompositeByteArrayBody = StringBody("""{
+                                                                   |    "contentId": 4,
+                                                                   |    "ticketMap": {
+                                                                   |        "TPCTT010000000000000000240": 2
+                                                                   |    }
+                                                                   |}""".stripMargin)
+
+  val ticketPurchaseBody = if (loadTestEnv.equals("dev") ) {
+    ticketPurchaseBodyDev
+  } else if (loadTestEnv.equals("prod") ) {
+    ticketPurchaseBodyProd
+  } else {
+    ticketPurchaseBodyDev
+  }
+
+  val ticketUseQueryMapProd = Map(
     "contentId" -> 18,
     "episodeId" -> 456,
     "sUsingWaitfree" -> false,
     "viewExpired" -> "NONE"
   )
+
+  val ticketUseQueryMapDev = Map(
+    "contentId" -> 4,
+    "episodeId" -> 7,
+    "sUsingWaitfree" -> false,
+    "viewExpired" -> "NONE"
+  )
+
+  val ticketUseQueryMap = if (loadTestEnv.equals("dev") ) {
+    ticketUseQueryMapDev
+  } else if (loadTestEnv.equals("prod") ) {
+    ticketUseQueryMapProd
+  } else {
+    ticketUseQueryMapDev
+  }
 }
--- src/gatling/scala/csvfeed/TicketPurchase.scala
+++ src/gatling/scala/csvfeed/TicketPurchase.scala
@@ -4,51 +4,38 @@ import baseConfig.IdnMembershipApiSimulation
 import io.gatling.commons.stats.OK
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import scala.concurrent.duration.DurationInt
 
 class TicketPurchase extends IdnMembershipApiSimulation {
 
-  val csvFeeder = csv("migratedUserNamesDev.csv").circular
-
-
+  val csvFeeder = csv("migratedUserNames-" + loadTestEnv.toLowerCase + ".csv").circular
 
   def postGiftGiveEventCash() = {
-//    feed(csvFeeder).
     exec(http("Post Gift Give Event Cash")
         .post("/internal/membership/v1/gift/give-event-cash")
         .header("Content-Type", "application/json")
-        .body(StringBody(
-        """{ "campaignId": 1,
-           "campaignName": "testCampaign",
-           "endDt": "2020-01-30T08:56:59Z",
-           "rewardAmount": 100,
-           "rewardId": 1,
-           "userId": "${id}" }""".stripMargin))
+        .body(giftGiveEventCashBody)
         .check(status.is(200))
         .check(jsonPath("$.resultMessage").is("success"))
-
     )
   }
 
   def getBalance() = {
-//    repeat(1) {
-//      feed(csvFeeder).
-        exec(http("Get Specific User")
-            .get("api/membership/v1/cash/balance")
-            .header("user-id", "${id}")
-            .check(jsonPath("$.result").greaterThan("-1"))
-            .check(status.is(200)))
-//        .pause(1)
-//    }
+    exec(http("Get Balance")
+        .get("api/membership/v1/cash/balance")
+        .header("user-id", "${id}")
+        .check(jsonPath("$.result").greaterThan("0"))
+        .check(status.is(200)))
   }
-
-
 
   def postTicketPurchase() = {
     exec(http("Post Ticket Purchase")
         .post("api/membership/v1/ticket/purchase")
         .header("user-id", "${id}")
+        .header("Content-Type", "application/json")
         .body(ticketPurchaseBody)
         .check(status.is(200))
+        .check(jsonPath("$.resultCode").is("200"))
     )
   }
 
@@ -56,48 +43,55 @@ class TicketPurchase extends IdnMembershipApiSimulation {
     exec(http("Post Ticket Use")
         .post("api/membership/v1/ticket/use")
         .header("user-id", "${id}")
+        .header("Content-Type", "application/json")
         .queryParamMap(ticketUseQueryMap)
         .check(status.is(200))
+        .check(jsonPath("$.resultCode").is("200"))
     )
   }
 
   def getViewers() = {
     exec(http("Get Viewer End")
-        .get("api/membership/v1/viewer/episode/" + episodeId.toString +"/end")
+        .get("api/membership/v1/viewer/episode/" + ticketUseQueryMap("episodeId").toString +"/end")
         .header("user-id", "${id}")
     ).
     exec(http("Get Viewer Meta")
-        .get("api/membership/v1/viewer/episode/" + episodeId.toString +"/meta")
+        .get("api/membership/v1/viewer/episode/" + ticketUseQueryMap("episodeId").toString +"/meta")
         .header("user-id", "${id}")
     ).
     exec(http("Get Viewer Download Data")
       .get("api/membership/v1/viewer/get-download-data")
       .header("user-id", "${id}")
         .queryParam("resourceType","ZIP")
-        .queryParam("episodeId", episodeId.toString())
+        .queryParam("episodeId", ticketUseQueryMap("episodeId").toString())
     )
   }
 
   def execAllSimulations() = {
-    repeat(2) {
+    repeat(1) {
       feed(csvFeeder)
-        //.exec(postGiftGiveEventCash())
+        .exec(postGiftGiveEventCash())
         .exec(getBalance())
         .exec(postTicketPurchase())
         .doIfOrElse( session => session.status == OK ) {
           exec(postTicketUse())
-            .exitHereIfFailed
-            .exec(getViewers())
+            .doIfOrElse( session => session.status == OK) {
+              exec(getViewers())
+            } {
+              exec { session =>
+                // here some println can be entered to do some debuggin, but it may slow-down the process
+                session
+              }
+            }
+
         } {
           exec { session =>
             println("Else Case Triggered session >>>>>>>> " + session)
-            println("episodeId: " + episodeId)
+            println("episodeId: " + ticketUseQueryMap("episodeId"))
             println("ticketPurchaseBody: " + ticketPurchaseBody.bytes.toString())
             session
           }
         }
-
-
     }
   }
 
@@ -106,7 +100,8 @@ class TicketPurchase extends IdnMembershipApiSimulation {
 
 
   setUp(
-    scn.inject(atOnceUsers(1))
+    //scn.inject(atOnceUsers(2))
+    scn.inject(constantUsersPerSec(2) during (10 seconds))
   ).protocols(httpConf)
 
 }
